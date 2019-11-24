22b7bb23c0c430cf83dbef67a425ea3d0b028088
xunillinux <jonas@meise.ch>
2019-11-24 23:43:59 +0100

    continued implementing sensorscripts, added logger class

--- sensorScripts/BrightnessSensor.py
+++ sensorScripts/BrightnessSensor.py
@@ -1,33 +1,34 @@
 import RPi.GPIO as GPIO
 import time
 
-GPIO.setmode(GPIO.BOARD)
-#define the pin that goes to the circuit
-pin_to_circuit = 7
+class BrightnessSensor:
 
-def rc_time (pin_to_circuit):
-	count = 0
+    def __init__(self):
+        self.pin_to_circuit = 7
 
-	#Output on the pin for
-	GPIO.setup(pin_to_circuit, GPIO.OUT)
-	GPIO.output(pin_to_circuit, GPIO.LOW)
-	time.sleep(0.1)
+    def Measure(self):
+		GPIO.setmode(GPIO.BOARD)
+        #TODO measure few times over a second and take average
+		print(self.rc_time(pin_to_circuit))
+		GPIO.cleanup()
 
-	#Change the pin back to input 
-	GPIO.setup(pin_to_circuit, GPIO.IN)
 
-	#Count until the pin goes high
-	while (GPIO.input(pin_to_circuit) == GPIO.LOW):
-     		count += 1
-	return count
-#Catch when script is interrupted, cleanup correctly
-try:
-    # Main loop
-    while True:
-        print(rc_time(pin_to_circuit))
-except KeyboardInterrupt:
-    pass
+	def rc_time (self, pin_to_circuit):
+		count = 0
 
-finally:
-	GPIO.cleanup()
+		#Output on the pin for
+		GPIO.setup(pin_to_circuit, GPIO.OUT)
+		GPIO.output(pin_to_circuit, GPIO.LOW)
+		time.sleep(0.1)
 
+		#Change the pin back to input 
+		GPIO.setup(pin_to_circuit, GPIO.IN)
+
+		#Count until the pin goes high
+		while (GPIO.input(pin_to_circuit) == GPIO.LOW):
+				count += 1
+		return count
+
+    def Log(self):
+        #TODO
+        print("TODO")
