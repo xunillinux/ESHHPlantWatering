cfa09601b9c377fa9ec9860e00a2ade28a71449d
xunillinux <jonas@meise.ch>
2019-11-24 23:25:16 +0100

    continued handler implementation

--- Handler/Handler.py
+++ Handler/Handler.py
@@ -3,10 +3,14 @@ sys.path.append('.')
 sys.path.append('./api')
 sys.path.append('./sensorScripts')
 
+import json
+
 from Controller import Controller
 from HumiditySensor import HumiditySensor
 from BrightnessSensor import BrightnessSensor
 from TemperatureSensor import TemperatureSensor
+from Camera import Camera
+from Pump import Pump
 
 class Handler:
 
@@ -15,32 +19,52 @@ class Handler:
         self.humiditySensor = HumiditySensor()
         self.brightnessSensor = brightnessSensor()
         self.temperatureSensor = TemperatureSensor()
+        self.camera = Camera()
+        self.pump = Pump()
+
+    def ExecuteAllSensors(self):
+        self.ExecuteBrightnessSensor()
+        self.ExectueTemperatureSensor()
+        self.ExecuteCamera()
+        humidity = self.ExecuteHumiditySensor()
+        if(self.HumidityBelowThreshhold()):
+            self.ExecutePump()
     
     def ExecuteHumiditySensor(self):
         humidityValue = self.humiditySensor.Measure()
         self.controller.AddHumidityValue(humidityValue)
+        #TODO LOG
         return humidityValue
     
     def ExecuteBrightnessSensor(self):
         brightnessValue = self.brightnessSensor.Measure()
         self.controller.AddBrightnessValue(brightnessValue)
+        #TODO LOG
         return brightnessValue
     
     def ExectueTemperatureSensor(self):
         temperatureValue = self.temperatureSensor.Measure()
         self.controller.AddTemperatureValue(temperatureValue)
+        #TODO LOG
         return temperatureValue
     
     def ExecuteCamera(self):
         photo = self.camera.TakePhoto()
         self.controller.SetPhoto(photo)
-        #TODO
-        print("TODO")
+        #TODO LOG
     
     def ExecutePump(self):
-        #TODO
-        print("TODO")
+        settings = json.loads(self.controller.GetSettings())
+        waterAmount = settings[0]["pump_water_amount"]
+        pumpTime = self.pump.ConvertWaterAmountInCLToSeconds(waterAmount)
+        self.pump.ExecuteForSeconds(pumpTime)
+        #TODO LOG
     
+    def HumidityBelowThreshhold(self, humidity):
+        settings = json.loads(self.controller.GetSettings())
+        humidityThreshhold = settings[0]["humidity_threshhold"]
+        return humidityValue < humidityThreshhold
+
     def Log(self):
         #TODO
         print("TODO")
---
+++ sensorScripts/Pump.py
@@ -0,0 +1,13 @@
+
+class Pump:
+
+    def ExecuteForSeconds(self, seconds):
+        #TODO
+        print("TODO")
+
+    def ConvertWaterAmountInCLToSeconds(waterAmountCl):
+        return waterAmountCl / 2.5
+
+    def Log(self):
+        #TODO
+        print("TODO")
