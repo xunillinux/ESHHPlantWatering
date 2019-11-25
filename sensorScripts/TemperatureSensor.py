e7b4475c6e2611cecd6aca9067c8a0e01ef9a5ca
Jonas Meise <jonas.meise@students.ffhs.ch>
2019-11-25 09:14:53 +0100

    fixed tabs spaces

--- sensorScripts/TemperatureSensor.py
+++ sensorScripts/TemperatureSensor.py
@@ -3,14 +3,14 @@ import Adafruit_DHT
 class TemperatureSensor:
 
 	def __init__(self):
-    	self.sensor = Adafruit_DHT.DHT11
-        self.PIN = 4
+		self.sensor = Adafruit_DHT.DHT11
+		self.PIN = 4
 
-    def Measure(self):
-        #TODO measure few times over a second and take average
-        humidity, temperature = Adafruit_DHT.read_retry(self.sensor, self.PIN)
-        return temperature
+	def Measure(self):
+		#TODO measure few times over a second and take average
+		humidity, temperature = Adafruit_DHT.read_retry(self.sensor, self.PIN)
+		return temperature
 
-    def Log(self):
-        #TODO
-        print("TODO")
+	def Log(self):
+		#TODO
+		print("TODO")
