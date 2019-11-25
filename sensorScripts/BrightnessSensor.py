63f3697044b5994ecfe5b2285cd9713c611cc10e
Jonas Meise <jonas.meise@students.ffhs.ch>
2019-11-25 08:55:07 +0100

    replaced spaces with tabs

--- sensorScripts/BrightnessSensor.py
+++ sensorScripts/BrightnessSensor.py
@@ -3,28 +3,28 @@ import time
 
 class BrightnessSensor:
 
-    def __init__(self):
-        self.pin_to_circuit = 7
+	def __init__(self):
+		self.pin_to_circuit = 7
 
-    def Measure(self):
+	def Measure(self):
 		GPIO.setmode(GPIO.BOARD)
-        #TODO measure few times over a second and take average
+		#TODO measure few times over a second and take average
 		#TODO convert into some brightness value
 		value = self.Rc_time(self.pin_to_circuit)
 		GPIO.cleanup()
 		return value
 
 	def Rc_time(self, pin_to_circuit):
-        count = 0
-        GPIO.setup(pin_to_circuit, GPIO.OUT)
-        GPIO.output(pin_to_circuit, GPIO.LOW)
-        time.sleep(0.1)
-        GPIO.setup(pin_to_circuit, GPIO.IN)
-        #Raise counter until pin goes high
-        while(GPIO.input(pin_to_circuit) == GPIO.LOW):
-            count += 1
-        return count
+		count = 0
+		GPIO.setup(pin_to_circuit, GPIO.OUT)
+		GPIO.output(pin_to_circuit, GPIO.LOW)
+		time.sleep(0.1)
+		GPIO.setup(pin_to_circuit, GPIO.IN)
+		#Raise counter until pin goes high
+		while(GPIO.input(pin_to_circuit) == GPIO.LOW):
+			count += 1
+		return count
 
-    def Log(self):
-        #TODO
-        print("TODO")
+	def Log(self):
+		#TODO
+		print("TODO")
