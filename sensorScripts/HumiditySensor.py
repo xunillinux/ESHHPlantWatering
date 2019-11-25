d79079b206838cd7535726775e4ad001f641e7d5
Jonas Meise <jonas.meise@students.ffhs.ch>
2019-11-25 12:04:44 +0100

    fixed HumiditySensor

--- sensorScripts/HumiditySensor.py
+++ sensorScripts/HumiditySensor.py
@@ -2,15 +2,14 @@ from MCP3008 import MCP3008
 
 class HumiditySensor:
 
-    def __init__(self):
-        self.adc = MCP3008()
-    
-    def Measure(self):
-        #TODO measure few times over a second and take average
-        value = self.adc.read( channel = 0 )
-	print (value)
-        convertedValue = value / 1023.0 * 3.3
+	def __init__(self):
+		self.adc = MCP3008()
+	
+	def Measure(self):
+		#TODO measure few times over a second and take average
+		value = self.adc.read( channel = 0 )
+		return value
 
-    def Log(self):
-        #TODO
-        print("TODO")
+	def Log(self):
+		#TODO
+		print("TODO")
